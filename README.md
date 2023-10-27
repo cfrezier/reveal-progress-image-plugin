6fb227456e37410331fc56b745e98a18fe5e4bc7
Christophe Frézier <frezier@gmail.com>
2023-10-27 17:33:12 +0200

    Publishing plugin

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # reveal-progress-image-plugin
 Add an image in your reveal.js progress bar
+
+## Configution
+
+By defaut configuration is set to:
+````js
+    iconSize: [30, 30],
+    bottom: 5,
+    moveTransition: 'left .8s cubic-bezier(.26,.86,.44,.985)',
+    imageSrc: './images/progress.webp',
+    imageAnimation: [
+        { transform: 'rotateY(0deg)' },
+        { transform: 'rotateY(360deg)' }
+    ]
+````
+
+Please make sure to include a file at ./images/progress.web or change the parameter
---
+++ dist/index.js
@@ -0,0 +1,29 @@
+const RevealProgressImage = ((Reveal) => {
+    return {
+        id: 'progressImage',
+        init: (reveal) => {
+            let config = Object.assign({ iconSize: [30, 30], bottom: 5, moveTransition: 'left .8s cubic-bezier(.26,.86,.44,.985)', imageSrc: 'https://en.m.wikipedia.org/wiki/File:Angular_full_color_logo.svg', imageAnimation: [
+                    { transform: 'rotateY(0deg)' },
+                    { transform: 'rotateY(360deg)' }
+                ] }, reveal.getConfig().progressImage);
+            const progressImage = document.createElement('img');
+            progressImage.style.position = 'absolute';
+            progressImage.style.maxHeight = config.iconSize[0] + 'px';
+            progressImage.style.maxWidth = config.iconSize[1] + 'px';
+            progressImage.style.bottom = config.bottom + 'px';
+            progressImage.style.transition = config.moveTransition;
+            progressImage.src = config.imageSrc;
+            if (config.imageAnimation) {
+                progressImage.animate(config.imageAnimation, {
+                    duration: 1000,
+                    iterations: Infinity
+                });
+            }
+            document.body.appendChild(progressImage);
+            Reveal.on('slidechanged', () => {
+                progressImage.style.left = (Reveal.getProgress() * 100) + '%';
+            });
+        }
+    };
+});
+export default RevealProgressImage;
