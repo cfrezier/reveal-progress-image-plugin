01ac1881cb3e46e43b1fa58e8af7e0c0f828729e
Christophe Frézier <frezier@gmail.com>
2023-10-28 14:59:30 +0200

    Adding default transition, fixing image global position

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,38 @@
 const RevealProgressImage = ((Reveal) => {
+    const ANIMATIONS = {
+        FLIP: [
+            {transform: 'rotateY(0deg)'},
+            {transform: 'rotateY(360deg)'}
+        ],
+        FLY: [
+            {transform: 'translateX(0px)'},
+            {transform: 'translateX(5px)'},
+            {transform: 'translateY(5px)'},
+            {transform: 'translateX(0px)'},
+        ],
+        DANCE: [
+            {transform: 'rotate(-20deg)'},
+            {transform: 'rotate(20deg)'},
+            {transform: 'rotate(-20deg)'},
+        ],
+        MOVE: [
+            { transform: 'translateX(0px)' },
+            { transform: 'translateX(5px)' },
+            { transform: 'translateX(0px)' },
+        ]
+    };
     return {
         id: 'progressImage',
+        ANIMATIONS,
         init: (reveal) => {
-            let config = Object.assign({ iconSize: [30, 30], bottom: 5, moveTransition: 'left .8s cubic-bezier(.26,.86,.44,.985)', imageSrc: './images/progress.webp', imageAnimation: [
-                    { transform: 'rotateY(0deg)' },
-                    { transform: 'rotateY(360deg)' }
-                ] }, reveal.getConfig().progressImage);
+            let config = Object.assign({
+                iconSize: [30, 30],
+                bottom: 5,
+                moveTransition: 'left .8s cubic-bezier(.26,.86,.44,.985)',
+                imageSrc: './images/progress.webp',
+                imageAnimation: ANIMATIONS.DANCE,
+                animationDuration: 1000
+            }, reveal.getConfig().progressImage);
             const progressImage = document.createElement('img');
             progressImage.style.position = 'absolute';
             progressImage.style.maxHeight = config.iconSize[0] + 'px';
@@ -15,15 +42,20 @@ const RevealProgressImage = ((Reveal) => {
             progressImage.src = config.imageSrc;
             if (config.imageAnimation) {
                 progressImage.animate(config.imageAnimation, {
-                    duration: 1000,
+                    duration: config.animationDuration,
                     iterations: Infinity
                 });
             }
             document.body.appendChild(progressImage);
-            Reveal.on('slidechanged', () => {
-                progressImage.style.left = (Reveal.getProgress() * 100) + '%';
-            });
-        }
+            const updatePosition = () => {
+                const progress = Reveal.getProgress() * 100;
+                progressImage.style.left = `calc(${progress}% - ${config.iconSize[0] * progress}px)'`;
+            }
+
+            Reveal.on('ready', updatePosition);
+            Reveal.on('resize', updatePosition);
+            Reveal.on('slidechanged', updatePosition);
+        },
     };
 });
 export default RevealProgressImage;
